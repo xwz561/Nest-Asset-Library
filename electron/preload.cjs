a1948b2b8516fabdc09a3421d346671951c17f7d
xwz561 <xwz561@users.noreply.github.com>
2026-08-26 21:53:18 +0800

    Improve clipboard audio hover and in-app feedback

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('nestDesktop', {
   currentLibrary: () => ipcRenderer.invoke('library:current'), createLibrary: () => ipcRenderer.invoke('library:create'), openLibrary: () => ipcRenderer.invoke('library:open'),
   health: () => ipcRenderer.invoke('library:health'), repair: () => ipcRenderer.invoke('library:repair'),
   prepareExtension: browser => ipcRenderer.invoke('extension:prepare', browser),
+  openBugFeedback: () => ipcRenderer.invoke('app:open-bug-feedback'),
   openAsset: id => ipcRenderer.invoke('library:open-asset', id), revealAsset: (id, migrate = false) => ipcRenderer.invoke('library:reveal-asset', id, migrate),
   startExternalDrag: ids => ipcRenderer.send('library:start-external-drag', ids),
   onExternalDragError: callback => { const listener = (_, message) => callback(message); ipcRenderer.on('library:external-drag-error', listener); return () => ipcRenderer.removeListener('library:external-drag-error', listener); },
